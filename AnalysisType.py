03ff49616baea224e49419e4b33b4896586d6405
SACCAP\ralphd <ralph.desiderio@point72.com>
2017-12-06 11:13:49 -0500

    Pulled out the loop through the chunks in order to put in different senarios

--- AnalysisType.py
+++ AnalysisType.py
@@ -1,6 +1,7 @@
 '''this function will take 2 columns, one will be the x axis (label- like country or hotel)
    and a colmn with numbers and adds the total'''
 
+# receives a label and a value and totals to provide a sum of that value
 def TypeLabelandTotal(row, col, col2, counts_dict2):
     if row[col] in counts_dict2.keys():
         counts_dict2[row[col]] += row[col2]
@@ -8,7 +9,7 @@ def TypeLabelandTotal(row, col, col2, counts_dict2):
         counts_dict2[row[col]] = row[col2]
 
     return counts_dict2
-
+# receives a label and then counts that label
 def TypeCount (row,col,counts_dict2):
     if row[col] in counts_dict2.keys():
         counts_dict2[row[col]] += 1
