33673c30f61e4c17564b87a8e18c84bd402278e0
SACCAP\ralphd <ralph.desiderio@point72.com>
2017-12-07 10:34:30 -0500

    add * args and some cosmetic cleanup

---
+++ Output.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+def GraphOutput(result_counts):
+
+    plt.bar(range(len(result_counts)), result_counts.values(), align='center',color = 'red')
+    plt.xticks(range(len(result_counts)), list(result_counts.keys()))
+    plt.xticks(rotation='vertical', fontsize = 5)
+    plt.title('Vegas Hotel Survey', fontsize=10, color = 'blue')
+
+
+
+    plt.show()
